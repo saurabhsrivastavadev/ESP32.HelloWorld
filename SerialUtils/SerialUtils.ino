9c56ef582b162d128a2bd039c7bd407fdafbebdd
Saurabh Srivastava <me@saurabhsrivastava.dev>
2022-07-10 19:54:28 +0530

    Adding SerialUtils :: serialInputBlocking

--- SerialUtils/SerialUtils.ino
+++ SerialUtils/SerialUtils.ino
@@ -1,20 +1,49 @@
 /**
-    HelloWorld.ino
+    SerialUtils.ino
     Created on: 10.Jul.2022
 
     Serial Port communication utilities.
 */
 
 #include <Arduino.h>
+#include <string>
+#include <sstream>
 
 void prepareSerial() {
 
   Serial.begin(115200);
+  delay(1000);
+  Serial.print("Preparing Serial.");
   for (uint8_t i = 0; i < 4; i++) {
-    Serial.println();
+    Serial.print(".");
     Serial.flush();
     delay(1000);
   }
+  Serial.println();
+}
+
+// Function to block for serial input 
+std::string serialInputBlocking(const std::string& msg, char terminatingChar = '\n') {
+  
+  Serial.print(msg.c_str());
+
+  char c = '\0';
+  std::stringstream ss;
+
+  while (c != terminatingChar) {
+
+    while (!Serial.available()) {
+      delay(100);
+    }
+    c = (char)Serial.read();
+    if (c != terminatingChar) {
+      ss << c;
+      Serial.print(c);      
+    }
+  }
+
+  Serial.println();
+  return ss.str();
 }
 
 // SETUP and LOOP
@@ -27,14 +56,7 @@ void loop() {
 
   Serial.println("In the loop.");
 
-  if (Serial.available()) {
-    Serial.println("Serial available:");
-    while (Serial.available()) {
-      Serial.print(char(Serial.read()));      
-    }
-  } else {
-    Serial.println("Serial unavailable");
-  }
+  serialInputBlocking("Please type your name: ");
 
   Serial.println("Going to sleep for 10 seconds..");
 
